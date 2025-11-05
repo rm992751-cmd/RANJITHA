3fa88c6749f9a5ab813b62c446aa12369ca88702
rm992751-cmd <rm992751@gmail.com>
2025-11-05 12:44:16 +0530

    Add files via upload

---
+++ basic-1.by.py
@@ -0,0 +1,20 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/JET3/Desktop/v. ranjitha/basic-1.py ============
+>>> integer_1
+10
+>>> integer_2
+0
+>>> float_1
+12.3
+>>> float_2
+-5.2
+>>> string_1
+'this is a sentence'
+>>> string_2
+'and this another'
+>>> Boolean_1
+True
+>>> Boolean_2
+False
