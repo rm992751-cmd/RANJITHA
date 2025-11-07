0fba4d147cb1751a55c3c10fd959bb621283fe41
rm992751-cmd <rm992751@gmail.com>
2025-11-07 15:21:12 +0530

    Add files via upload

---
+++ WHILE CONDITION.py
@@ -0,0 +1,16 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============== RESTART: C:/Users/JET3/Desktop/v. ranjitha/WHILE.PY =============
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+>>> 
