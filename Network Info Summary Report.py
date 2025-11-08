7c1e979e1dcb6db706f59d31ff01355b4964f0e9
rm992751-cmd <rm992751@gmail.com>
2025-11-08 16:47:33 +0530

    Add files via upload

---
+++ Network Info Summary Report.py
@@ -0,0 +1,9 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/JET3/AppData/Local/Programs/Python/Python314/Network Info Summary Report.py
+===NETWORK INFO REPORT===
+Hostname: DESKTOP-UPESD0U
+Local IP; 192.168.1.76
+Platform: Windows 10
+MAC Address: 00:fe:bc:56:49:26
