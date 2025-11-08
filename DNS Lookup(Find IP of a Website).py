fdcf5fe5ca72c00a645ab04f47cff5c601930a85
rm992751-cmd <rm992751@gmail.com>
2025-11-08 14:10:27 +0530

    Add files via upload

---
+++ DNS Lookup(Find IP of a Website).py
@@ -0,0 +1,6 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/JET3/AppData/Local/Programs/Python/Python314/DNA Lookup(Find IP of a Website).py
+Enter domain name:www.youtube.com
+IP Address of is 142.251.223.238
