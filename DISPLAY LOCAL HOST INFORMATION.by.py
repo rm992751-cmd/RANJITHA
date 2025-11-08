edf8cc9911e09e9652a877a42a3478d40c9dc2c8
rm992751-cmd <rm992751@gmail.com>
2025-11-08 12:53:05 +0530

    Add files via upload

---
+++ DISPLAY LOCAL HOST INFORMATION.by.py
@@ -0,0 +1,6 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/JET3/Desktop/v. ranjitha/DISPLAY LOCAL HOST INFORMATEON.by.py
+Your Computer Name: DESKTOP-UPESD0U
+yourIP Address: 192.168.1.76
